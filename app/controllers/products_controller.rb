2b5899f3ac1cd4ad9dfb5cbff194fbdb7331ceb3
gmocaceres <guillecaceres1989@gmail.com>
2019-06-28 13:03:47 -0400

    vista de products en proceso, pendiente añadir gema faker e implementarla en el seeds para probar

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,17 @@
 class ProductsController < ApplicationController
   def index
+      @product = Product.all
   end
+
+    def show
+        @product = Product.find(params[:id]) 
+    end
+
+
+    private
+
+    def product_params
+        params.require(:product).permit(:name, :description, :image, :price)
+    end
+
 end
