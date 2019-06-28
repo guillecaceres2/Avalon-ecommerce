cefbc8f7feb7b206b95992756163813f3e33c924
gmocaceres <guillecaceres1989@gmail.com>
2019-06-28 15:50:24 -0400

    agregada table order y controller orders

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'orders/index'
+  get 'order/index'
   get 'products/index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
