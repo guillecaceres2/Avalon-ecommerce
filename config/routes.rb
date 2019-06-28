d9d1065f40ca83ddca64679ddd58a1fe6641d075
gmocaceres <guillecaceres1989@gmail.com>
2019-06-28 16:22:24 -0400

    úlitmo commit antes de subir a heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   get 'orders/index'
-  get 'order/index'
   get 'products/index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
