127a492fee720ae0dd0a59a911bb1550273f3215
gmocaceres <guillecaceres1989@gmail.com>
2019-06-30 23:45:15 -0400

    products controller corregido

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,6 @@
 class ProductsController < ApplicationController
+     before_action :set_product, only: [:show, :edit, :update, :destroy]
+
   def index
       @product = Product.all
   end
@@ -6,12 +8,61 @@ class ProductsController < ApplicationController
     def show
         @product = Product.find(params[:id]) 
     end
+      
+  def new
+    @product = Product.new
+  end
+
+    def edit
+  end
+
+
+ def create
+    @product = Product.new(product_params)
+
+    respond_to do |format|
+      if @product.save
+        format.html { redirect_to @product, notice: 'Product was successfully created.' }
+        format.json { render :show, status: :created, location: @product }
+      else
+        format.html { render :new }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @product.update(product_params)
+        format.html { redirect_to @product, notice: 'Product was successfully updated.' }
+        format.json { render :show, status: :ok, location: @product }
+      else
+        format.html { render :edit }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+def destroy
+    @product.destroy
+    respond_to do |format|
+      format.html { redirect_to products_url, notice: 'Product was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
 
 
     private
+
+    def set_product
+      @product = Product.find(params[:id])
+    end
 
     def product_params
         params.require(:product).permit(:name, :description, :image, :price)
     end
 
 end
+
+ 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'orders/index'
-  get 'products/index'
+  resources :products
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "products#index"
