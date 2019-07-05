a3c01ab66456dda577e5bbb3c952d4f61217f469
gmocaceres <guillecaceres1989@gmail.com>
2019-07-05 16:04:13 -0400

    detención de paypal para comenzar a integrar carro de compras

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   get 'orders/index'
+  resources :billings, only: [] do
+     collection do        
+      get 'pre_pay'
+    end
+  end
   resources :products
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
